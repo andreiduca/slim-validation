94a0009fa6d3ffd93b70e123fb116da9c3f4ec77
Andrei Duca <duca.andrei@gmail.com>
2015-09-01 17:46:04 +0300

    plugin closure

--- src/slim-validation.jquery.js
+++ src/slim-validation.jquery.js
@@ -1,115 +1,115 @@
-$.fn.ValidationRule = function (rule) {
-    return {
-        validate: rule
-    }
-};
+(function($) {
 
-$.fn.Validator = function () {
-    return {
-        skip: new $.fn.ValidationRule(function () {
-            return true;
-        }),
+    $.fn.ValidationRule = function (rule) {
+        return {
+            validate: rule
+        };
+    };
 
-        required: new $.fn.ValidationRule(function (value) {
-            return typeof value != 'undefined' && value != '';
-        }),
+    $.fn.Validator = function () {
+        return {
+            skip: new $.fn.ValidationRule(function () {
+                return true;
+            }),
 
-        requiredBy: $.extend(new $.fn.ValidationRule(function (value, param) {
-                return $(param).length == 0 || $(param).val() == '' || (typeof value != 'undefined' && value != '');
-            }), {
-                withParam: true
-            }
-        ),
+            required: new $.fn.ValidationRule(function (value) {
+                return typeof value !== 'undefined' && value !== '';
+            }),
 
-        minLength: $.extend(new $.fn.ValidationRule(function (value, param) {
-                return typeof value != 'undefined' && value.length >= parseInt(param);
-            }), {
-                withParam: true
-            }
-        ),
+            requiredBy: $.extend(new $.fn.ValidationRule(function (value, param) {
+                    return $(param).length === 0 || $(param).val() === '' || (typeof value !== 'undefined' && value !== '');
+                }), {
+                    withParam: true
+                }
+            ),
 
-        maxLength: $.extend(new $.fn.ValidationRule(function (value, param) {
-                return typeof value != 'undefined' && value.length <= parseInt(param);
-            }), {
-                withParam: true
-            }
-        ),
+            minLength: $.extend(new $.fn.ValidationRule(function (value, param) {
+                    return typeof value !== 'undefined' && value.length >= parseInt(param);
+                }), {
+                    withParam: true
+                }
+            ),
 
-        email: new $.fn.ValidationRule(function (value) {
-            // TODO: implement logic here
-            return false;
-        }),
+            maxLength: $.extend(new $.fn.ValidationRule(function (value, param) {
+                    return typeof value !== 'undefined' && value.length <= parseInt(param);
+                }), {
+                    withParam: true
+                }
+            ),
 
-        telephone: $.extend(new $.fn.ValidationRule(function (value) {
+            email: new $.fn.ValidationRule(function (value) {
                 // TODO: implement logic here
                 return false;
-            }), {
-                cleanVal: function (value) {
-                    return value.replace(/[\+\-\.\s\(\),#:]/gi, '')
+            }),
+
+            telephone: $.extend(new $.fn.ValidationRule(function (value) {
+                    // TODO: implement logic here
+                    return false;
+                }), {
+                    cleanVal: function (value) {
+                        return value.replace(/[\+\-\.\s\(\),#:]/gi, '');
+                    }
+                }
+            )
+        };
+    };
+
+    $.fn.validateInput = function () {
+        var $input = $(this);
+
+        var rules = ($input.attr('data-validate') || '').trim().replace(/[\s]{2,}/g, ' ').split(' ');
+
+        var inputName = $input.attr('id') || $input.attr('name');
+        var inputValue = $input.val().trim();
+        var isValid = true;
+        var errorMessage = null;
+        var Validator = new $.fn.Validator();
+
+        for (var i = 0; i < rules.length && isValid; i++) {
+            var rule = rules[i], param;
+
+            // stop further processing
+            if (rule === '' || rule === 'skip' || (rule === 'optional' && inputValue === '')) {
+                break;
+            }
+
+            if (typeof Validator[rule] !== 'undefined') {
+
+                // rule needs further cleanliness of value
+                if (typeof Validator[rule].cleanVal === 'function') {
+                    inputValue = Validator[rule].cleanVal(inputValue);
+                }
+
+                // treat next rule as a parameter for current rule
+                if (Validator[rule].withParam) {
+                    param = rules[++i];
+                }
+
+                // validate the input for current rule
+                if (!Validator[rule].validate(inputValue, param)) {
+                    isValid = false;
+                    errorMessage = ($input.attr('data-error-' + rule) || $input.attr('data-error') || '');
                 }
             }
-        )
+        }
+
+        $input.val(inputValue);
+        $input.attr('data-is-valid', isValid);
+        $input.trigger('validation:end', [$input, inputValue, isValid, errorMessage]);
+
+        var dependent = $('[data-validate*="requiredBy #' + $input.attr('id') + '"]');
+        if (dependent.length > 0 && (inputName !== dependent.attr('id') || dependent.attr('name'))) {
+            dependent.validateInput();
+        }
+
+        return isValid;
     };
-};
-
-$.fn.validateInput = function () {
-    var $input = $(this);
-
-    var rules = ($input.attr('data-validate') || '').trim().replace(/[\s]{2,}/g, ' ').split(' ');
-
-    // stop further processing
-    if (rules[0] == '' || rules[0] == 'skip') {
-        return true;
-    }
-
-    var inputName = $input.attr('id') || $input.attr('name');
-    var inputValue = $input.val().trim();
-    var isValid = true;
-    var errorMessage = null;
-    var Validator = new $.fn.Validator();
-
-    for (var i = 0; i < rules.length && isValid; i++) {
-        var rule = rules[i], param;
-
-        if (rule == 'optional' && inputValue == '') {
-            break;
-        }
-
-        if (typeof Validator[rule] != 'undefined') {
-
-            // rule needs further cleanliness of value
-            if (typeof Validator[rule].cleanVal == 'function') {
-                inputValue = Validator[rule].cleanVal(inputValue);
-            }
-
-            // treat next rule as a parameter for current rule
-            if (Validator[rule].withParam) {
-                param = rules[++i];
-            }
-
-            // validate the input for current rule
-            if (!Validator[rule].validate(inputValue, param)) {
-                isValid = false;
-                errorMessage = ($input.attr('data-error-' + rule) || $input.attr('data-error') || '');
-            }
-        }
-    }
-
-    $input.val(inputValue);
-    $input.attr('data-is-valid', isValid);
-    $input.trigger('validation:end', [$input, inputValue, isValid, errorMessage]);
-
-    var dependent = $('[data-validate*="requiredBy #' + $input.attr('id') + '"]');
-    if (dependent.length > 0 && (inputName != dependent.attr('id') || dependent.attr('name'))) {
-        dependent.validateInput();
-    }
-
-    return isValid;
-};
 
 
-$(window).load(function () {
-    $('body').on('blur', '[data-validate]', function() {
-        $(this).validateInput();
+    $(window).load(function () {
+        $('body').on('blur', '[data-validate]', function() {
+            $(this).validateInput();
+        });
     });
-});
+
+})(jQuery);
